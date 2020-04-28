ea79ea38c67e4b4be6d741f9af4e45720eb47cbd
Ireaurora <is816@york.ac.uk>
2020-04-28 10:57:32 +0100

    Changs to the dashboard

--- Sepr/src/pages/Dashboard.js
+++ Sepr/src/pages/Dashboard.js
@@ -21,8 +21,8 @@ const Dashboard = ({ DeviceWidth }) => {
 
                <h1 style={{fontSize: '3.5rem', marginBottom: '1rem'}}>Play <strong style={{fontSize: '5rem'}}>Kroy!</strong></h1>
                 <h3 style={{ marginBottom: '1rem'}}>Created by <strong style={{fontSize: '2.5rem'}}>MiKroysoft!</strong></h3>
-                    <a href="https://drive.google.com/file/d/1bmw0JG5HgpIfrzvuSjhQQp50Sj79XysK/view?usp=sharing" rel="noopener noreferrer" target="_blank">
-                        <h5>Download the Mikroysoft game here - Version 3</h5>
+                <a href="" rel="noopener noreferrer" target="_blank">
+                        <h5>Download the Mikroysoft game here - Version assessment 4</h5>
                     </a>
                     <p>The following links contain details about our project</p>
                     <ul>
@@ -35,6 +35,9 @@ const Dashboard = ({ DeviceWidth }) => {
                     </ul>
                     <a href="https://drive.google.com/a/york.ac.uk/file/d/1yjQCnpAWV_J7Hk0eDUBXWFZsT8MuFupM/view?usp=sharing" rel="noopener noreferrer" target="_blank">
                         <h5>Salt and Sepr Assessment Two game - Version 2</h5>
+                    </a>
+                    <a href="https://drive.google.com/file/d/1bmw0JG5HgpIfrzvuSjhQQp50Sj79XysK/view?usp=sharing" rel="noopener noreferrer" target="_blank">
+                        <h5>Download the Mikroysoft game here - Version 3</h5>
                     </a>
                 
             </div>
